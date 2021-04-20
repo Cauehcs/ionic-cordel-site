d616ef67cb0d7a155962ef9c10b83a0f5d63f208
gustavosalm <gustavosalmeida9821@gmail.com>
2021-04-20 17:20:59 -0300

    transicao da parte de aparicoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor() {}
 
+  currentAppearence: number = 0;
+  interval: number;
+  appearences: string[] = ["Estudante cria empresa e desenvolve alternativas\nde ensino por meio de jogos",
+                           "Play for the planet: The favela youth redesigning gaming from Brazil’s north-east",
+                           "Primeira startup incubada no Porto Digital de Recife com alunos do Ensino Médio",
+                           "Startup embarcada no Porto Digital ganha reconhecimento internacional"];
+  appearenceFrom: string[] = ["Em Bom Dia PE",
+                              "Em Pionners Post",
+                              "Em NE 10",
+                              "Em Folha PE"];
+  appearenceImg: string[] = ["../../assets/fotobomdiape.png",
+                              "../../assets/fotopionnerspost.jpg",
+                              "../../assets/fotone10.jpg",
+                              "../../assets/Fotofolhape.jpg"];
+
+  ngOnInit() {
+    this.currentAppearence = 0;
+    console.log(this.currentAppearence);
+    this.interval = self.setInterval(() => {
+      document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.remove('checked');
+      this.currentAppearence += (this.currentAppearence == 3) ? -3 : 1;
+      document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.add('checked');
+    }, 10000);
+  }
+
+  changeApparition(index: number){
+    clearInterval(this.interval);
+    document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.remove('checked');
+    this.currentAppearence = index;
+    document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.add('checked');
+    this.interval = self.setInterval(() => {
+      document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.remove('checked');
+      this.currentAppearence += (this.currentAppearence == 3) ? -3 : 1;
+      document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.add('checked');
+    }, 10000);
+  }
 }
