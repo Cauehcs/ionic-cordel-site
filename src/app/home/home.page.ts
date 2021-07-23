baf037e4faff55681666b6702c072fe317dd8923
gustavosalm <gustavosalmeida9821@gmail.com>
2021-07-22 21:58:39 -0300

    testando o mailchimp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+const { mailChimp } = require('@mailchimp/mailchimp_marketing');
 
 @Component({
   selector: 'app-home',
@@ -52,33 +53,36 @@ export class HomePage implements OnInit{
       disableOnInteraction: false,
       },
   };
-  currentAppearence: number = 0;
+  currentAppearence = 0;
   interval: number;
-  appearences: string[] = ["Estudante cria empresa e desenvolve alternativas\nde ensino por meio de jogos",
-                           "Play for the planet: The favela youth redesigning gaming from Brazil’s north-east",
-                           "Primeira startup incubada no Porto Digital de Recife com alunos do Ensino Médio",
-                           "Startup embarcada no Porto Digital ganha reconhecimento internacional"];
-  appearenceFrom: string[] = ["Bom Dia PE",
-                              "Pionners Post",
-                              "NE 10",
-                              "Folha PE"];
-  appearenceImg: string[] =  ["../../assets/Aparicoes/fotobomdiape.png",
-                              "../../assets/Aparicoes/fotopionnerspost.jpg",
-                              "../../assets/Aparicoes/fotone10.jpg",
-                              "../../assets/Aparicoes/Fotofolhape.jpg"];
-  appearenceLink: string[] = ["https://www.folhape.com.br/colunistas/tecnologia-e-games/pernambucano-de-17-anos-cria-propria-startup-e-ganha-reconhecimento-internacional/20594/", 
-                              "https://www.pioneerspost.com/news-views/20200313/play-the-planet-the-favela-youth-redesigning-gaming-brazil-s-north-east", 
-                              "https://globoplay.globo.com/v/8885279/", 
-                              "https://jc.ne10.uol.com.br/pernambuco/2020/09/11978112-aos-17-anos--aluno-de-escola-publica-do-recife-se-torna-ceo-de-startup-que-nasceu-na-sala-de-aula.html"];
+  appearences: string[] = ['Estudante cria empresa e desenvolve alternativas\nde ensino por meio de jogos',
+                           'Play for the planet: The favela youth redesigning gaming from Brazil’s north-east',
+                           'Primeira startup incubada no Porto Digital de Recife com alunos do Ensino Médio',
+                           'Startup embarcada no Porto Digital ganha reconhecimento internacional'];
+  appearenceFrom: string[] = ['Bom Dia PE',
+                              'Pionners Post',
+                              'NE 10',
+                              'Folha PE'];
+  appearenceImg: string[] =  ['../../assets/Aparicoes/fotobomdiape.png',
+                              '../../assets/Aparicoes/fotopionnerspost.jpg',
+                              '../../assets/Aparicoes/fotone10.jpg',
+                              '../../assets/Aparicoes/Fotofolhape.jpg'];
+  appearenceLink: string[] = ['https://www.folhape.com.br/colunistas/tecnologia-e-games/pernambucano-de-17-anos-cria-propria-startup-e-ganha-reconhecimento-internacional/20594/',
+                              'https://www.pioneerspost.com/news-views/20200313/play-the-planet-the-favela-youth-redesigning-gaming-brazil-s-north-east',
+                              'https://globoplay.globo.com/v/8885279/',
+                              'https://jc.ne10.uol.com.br/pernambuco/2020/09/11978112-aos-17-anos--aluno-de-escola-publica-do-recife-se-torna-ceo-de-startup-que-nasceu-na-sala-de-aula.html'];
   currentHeaderClass = 'headerTop';
   headerIsOpened = false;
 
+  mailchimp = require('@mailchimp/mailchimp_marketing');
   ngOnInit() {
     this.currentAppearence = 0;
-    console.log(this.currentAppearence);
-    // this.interval = self.setInterval(() => {
-    //   console.log(document.getElementById('appContent').getScrollElement());
-    // }, 1000);
+    console.log(this.mailchimp);
+    this.mailchimp.setConfig({
+      apiKey: 'cdd615cddcbfa18637c3d4a25f33f183-us6',
+      server: 'us6',
+    });
+    this.run();
   }
 
   async changeHeader(event){
@@ -93,11 +97,9 @@ export class HomePage implements OnInit{
       this.currentHeaderClass = 'headerTop';
     }
   }
-
   slidesDidLoad(slides) {
     slides.startAutoplay();
   }
-
   changeApparition(index: number){
     clearInterval(this.interval);
     document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.remove('checked');
@@ -105,16 +107,22 @@ export class HomePage implements OnInit{
     document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.add('checked');
     this.interval = self.setInterval(() => {
       document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.remove('checked');
-      this.currentAppearence += (this.currentAppearence == 3) ? -3 : 1;
+      this.currentAppearence += (this.currentAppearence === 3) ? -3 : 1;
       document.getElementById('appearenceBtn' + this.currentAppearence.toString()).classList.add('checked');
     }, 10000);
   }
-
   openHeader(){
-    if(this.headerIsOpened)
+    if (this.headerIsOpened) {
       document.getElementById('SiteHeader').classList.remove('headerOpened');
-    else
+    }
+    else {
       document.getElementById('SiteHeader').classList.add('headerOpened');
+    }
     this.headerIsOpened = !this.headerIsOpened;
+  }
+  async run() {
+    const response = await this.mailchimp.ping.get().then((ping) => {
+      console.log(ping);
+    });
   }
 }
